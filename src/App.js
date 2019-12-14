e321d4fab67305e55e50a478caa23e7ba35ce73e
Rebecca Rangel <rebecca.metallica@hotmail.com>
2019-12-14 13:49:10 -0300

    Projeto react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'; 
+import { Tabela } from './styles';
+import {Cadastro} from './styles';
+import {Botao} from './styles';
+import {Input} from './styles';
+
+export default class CadastroUsuario extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      nome: "",
+      telefone: "",
+      email: "",
+      pessoas: [
+        {
+          nome: "Marcos",
+          email: "marcos@gmail.com",
+          telefone: "21-9999-9999"
+        },
+        {
+          nome: "Gustavo",
+          email: "gustavo@hotmail.com",
+          telefone: "21-9087-6542"
+        }
+      ]
+    }; 
+  }
+
+  render() { 
+    return (
+      <>
+        <form onSubmit={this.cadastrarUsuario}>
+
+          <Cadastro
+            id="input-nome"
+            type="text"
+            placeholder="Nome"
+            value={this.state.nome}
+            onChange={evento => { 
+              this.setState({ nome: evento.target.value });
+            }}
+          />
+
+          <Cadastro
+            id="input-email"
+            type="email"
+            placeholder="E-mail"
+            value={this.state.email}
+            onChange={evento => { 
+              this.setState({ email: evento.target.value });
+            }}
+          />
+
+          <Cadastro
+            id="input-tel"
+            type="tel"
+            placeholder="Telefone"
+            value={this.state.telefone}
+            onChange={evento => {
+              this.setState({ telefone: evento.target.value });
+            }}
+          />
+
+          <Input type="submit" />
+        </form>
+
+
+
+        <table>
+          <tr>
+            <Tabela>NOME</Tabela>
+            <Tabela>E-MAIL</Tabela>
+            <Tabela>TELEFONE</Tabela>
+          </tr>
+
+          
+
+          {this.state.pessoas.map((pessoa, index) => (
+            <tr key={index}>
+              <Tabela>{pessoa.nome}</Tabela>
+              <Tabela>{pessoa.email}</Tabela>
+              <Tabela>{pessoa.telefone}</Tabela>
+              <td><Botao onClick={() => this.removerUsuario(index)}>Remover</Botao></td>
+            </tr>
+          ))}
+
+        </table>
+
+      </>
+
+    );
+  }
+
+  
+
+ 
+  cadastrarUsuario = (evento) => {
+    evento.preventDefault();
+    this.setState({
+      pessoas: [...this.state.pessoas, { nome: this.state.nome, email: this.state.email, telefone: this.state.telefone }], 
+      nome: "",
+      email: "",
+      telefone: ""
+    });
+  }
+
+  removerUsuario = (index) => {
+    this.setState({
+      pessoas: [...this.state.pessoas.slice(0, index), ...this.state.pessoas.slice(index + 1)]
+    })
+  }
 }
 
-export default App;
+
+
---
+++ src/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Tabela = styled.td`
+    border-style: double;
+    background-color: #FFFAF0;   
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;
+`
+export const Cadastro=styled.input`
+    margin:8px 10px 8px 0px;
+    background-color: #FFFAF0;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;
+`
+export const Botao=styled.button`
+border-radius:5px 0px 0px 5px;
+text-transform: uppercase;
+background-color:#E9967A;
+font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;
+`
+
+export const Input=styled.input`
+border-radius: 5px 0px 0px 5px;
+text-transform: uppercase;
+background-color:#E9967A;
+font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;
+`
